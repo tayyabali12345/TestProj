2cc110db3095b29914ae9603a87a7f9a55440580
tayyabali12345 <tayab.ali@devsinc.com>
2023-08-11 19:03:00 +0500

    Here I add user profile functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :reactions, dependent: :destroy
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
